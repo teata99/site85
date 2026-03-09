08a03a71bdb5a4718ec10cccf2a65572a6e99e96
teata99 <teata99@naver.com>
2026-03-09 14:01:11 +0000

    update

--- api/routes/fastapi/fastapi01.py
+++ api/routes/fastapi/fastapi01.py
@@ -1,10 +1,21 @@
 import os
 from fastapi import APIRouter
+from pathlib import Path
+from dotenv import load_dotenv
 
-token = os.environ.get("site85_token")
+current_dir = Path(__file__).resolve().parent
+env_path = current_dir / ".env01"
+load_dotenv(dotenv_path=env_path)
+token = os.getenv("site85_token")
 
 router = APIRouter()
 
 @router.get("/")
 async def get_users():
-    return {"message": "유저 목록입니다.", "token:": token}
+    return {
+        "message": "유저 목록입니다...", 
+        "token": token,
+        "current_dir": current_dir, 
+    }
+
+
