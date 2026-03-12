e2f0dabb63ee1132bb2f7dd548987a4511f0cdbd
teata99 <teata99@naver.com>
2026-03-12 15:27:26 +0000

    update content.js

--- api/cloudflare/content.js
+++ api/cloudflare/content.js
@@ -4,6 +4,14 @@ export default async function handler(req, res) {
   if (cookies.is_human === 'true') {
     return res.status(200).send('<h1>🎉 인증 완료! 비밀 콘텐츠입니다.</h1>');
   } else {
+    
+    const cookiesToClear = ['is_human', 'cf_clearance']; 
+    const clearCookieHeaders = cookiesToClear.map(name => 
+      `${name}=; Path=/; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Lax`
+    );
+    
+    res.setHeader('Set-Cookie', clearCookieHeaders);
+
     return res.status(401).send('인증이 필요합니다.');
   }
 }
